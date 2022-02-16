b878f5299ce6d4fecd24d56c61e2c54a99543be0
Odofin Oyejide <odofin@swipe.ng>
2022-02-16 17:10:23 +0100

    Exposed the business logic and endpoints. This consists of executeScript,listLanguage,listPaginatedTask and listTopThreeSuccess methods.

---
+++ src/main/java/cognizant/com/codechallenge/route/AppRoute.java
@@ -0,0 +1,38 @@
+package cognizant.com.codechallenge.route;
+
+import cognizant.com.codechallenge.dto.CompileUiPayload;
+import cognizant.com.codechallenge.service.CompilerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static cognizant.com.codechallenge.utils.AppConstants.*;
+
+@RestController
+@RequestMapping("/api")
+public class AppRoute {
+    @Autowired
+    private  CompilerService compilerService;
+
+    @PostMapping("/script/execute")
+    public ResponseEntity executeScript(@Valid @RequestBody CompileUiPayload payload){
+        return compilerService.executeScript(payload);
+    }
+    @GetMapping("/script/languages")
+    public ResponseEntity listLanguage() {
+        return compilerService.listLanguage();
+    }
+    @GetMapping("/task/list")
+    public ResponseEntity listPaginatedTask(@RequestParam(value=PAGE_PARAM, defaultValue = PAGE) int page,
+                                            @RequestParam(value=SIZE_PARAM, defaultValue = SIZE) int size) {
+        return compilerService.listPaginatedTask(page,size);
+    }
+    @GetMapping("/task/report")
+    public ResponseEntity listTopThreeSuccess() {
+        return compilerService.listTopThreeSuccess();
+    }
+
+
+}
